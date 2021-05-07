33ab09559a6714acd9f6da9fb17ecafb9290ffe3
@Edwin-Castro_Gr <edwincas_17@hotmail.com>
2021-05-06 23:37:12 -0500

    --- Auto-Git Commit ---

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -111,7 +111,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if(numero % 1 == 0){
+  if(umero - Math.floor(numero) == 0){
     return true;
   }else {
     return false;
@@ -124,6 +124,14 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero % 15 === 0) {
+    return 'fizzbuzz';
+  } if(numero % 3 === 0) {
+     return 'fizz';
+    }if(numero % 5 === 0) {
+      return 'buzz';
+    }
+  return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -132,8 +140,23 @@ function operadoresLogicos(num1, num2, num3) {
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-}
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  else {
+    return false;
+  }
+} 
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
@@ -141,6 +164,11 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero===2) {
+    return true;    
+  } else {
+    
+  }
 }
 
 function esVerdadero(valor){
