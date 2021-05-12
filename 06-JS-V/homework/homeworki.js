4b4b211cc495c026c5d54c996f500c373e8de9ad
@Edwin-Castro_Gr <edwincas_17@hotmail.com>
2021-05-12 10:07:38 -0500

    --- Auto-Git Commit ---

--- 06-JS-V/homework/homeworki.js
+++ 06-JS-V/homework/homeworki.js
@@ -37,7 +37,13 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  
+  String.prototype.reverse = function () {
+    var stringInvertida = '';
+    for (let i = 0; i < this.length; i--) {
+      stringInvertida = stringInvertida + this.charAt(i);
+    }
+    return stringInvertida;
+  };
 }
 
 // ---------------------------------------------------------------------------//
@@ -52,9 +58,21 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor(nombre,apellido,edad,domicilio) {
       // Crea el constructor:
-
+      
+      this.nombre = nombre,
+      this.apellido = apellido,
+      this.edad = edad,
+      this.domicilio = domicilio
+      this.detalle = function () {
+        return{
+          Nombre : this.nombre,
+          Apellido : this.apellido,
+          Edad : this.edad,
+          Domicilio : this.domicilio 
+        }
+      }
     }
 }
 
@@ -62,13 +80,17 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  const persona = new Persona(nombre, apellido, edad, dir);
+  return persona;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
-}
-  
+  Persona.prototype.datos = function() {
+    return this.nombre + ", " + this.edad + " años";
+};
+} 
 
 // No modificar nada debajo de esta línea
 // --------------------------------
